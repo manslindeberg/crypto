3c04b02e427237997e9e389a9788a73c2a5dc12a
Måns Lindeberg <ma5671li-s@student.lu.se>
2021-12-07 10:43:23 +0100

    attack complete

--- project3/corr_attack.py
+++ project3/corr_attack.py
@@ -16,7 +16,7 @@ def lfsr(taps: List[int], init_state: List[int], length: int) -> List[int]:
 	for _ in range(0, length):
 		new_val = 0
 		for tap in taps:
-			new_val += state[tap-1] 
+			new_val += state[len(init_state) - tap] 
 		new_val = new_val % 2
 		sequence.append(state.pop(0))
 		state.append(new_val)
@@ -71,14 +71,22 @@ def bin_to_list(binary_string: str) -> List[int]:
 			return -1
 	return binary_list
 
-def hamming2(first, second):
-	assert len(first) )) len(second)
-	return sum(c1 != c2 for c1, c2 in zip(first, second)
+def find_biggest_dev(correlations: List[int]) -> int:
+	biggest = 0
+	index_of_biggest = 0
+	for index, corr in enumerate(correlations):
+		if abs(0.5 - corr) > biggest:
+			index_of_biggest = index
+			biggest = abs(0.5 - corr)
+	return correlations[index_of_biggest]
+		
+	
 if __name__ == '__main__':
 	c1 = [1,2,4,6,7,10,11,13]
 	c2 = [2,4,6,7,10,11,13,15]
 	c3 = [2,4,5,8,10,13,16,17]
-		
+	c5 = [1,3]
+
 	c1_length = 13
 	c2_length = 15
 	c3_length = 17
@@ -101,12 +109,13 @@ if __name__ == '__main__':
 		est_correlations_1.append(correlation)
 		
 	est_correlations_np = np.array(est_correlations_1)	
-	#plt.plot(np.array([i for i in range(1,2**13)]), est_correlations_np) 
+	#plt.plot(np.array([i for i in range(1,2**13)]), est_correlations_np, 'g^') 
 	#plt.show()
+		
+	biggest_val = find_biggest_dev(est_correlations_1)
+	print(f'Estimated biggest correlation 1: {biggest_val}')
 	
-	print(f'Estimated initial state 1: {max(est_correlations_1)}')
-	
-	c1_init_state = est_correlations_1.index(max(est_correlations_1)) + 1
+	c1_init_state = est_correlations_1.index(biggest_val) + 1
 	
 	est_correlations_2 = []
 	for init_state in range(1, 2**15):
@@ -116,12 +125,13 @@ if __name__ == '__main__':
 		est_correlations_2.append(correlation)	
 	
 	est_correlations_np = np.array(est_correlations_2)	
-	plt.plot(np.array([i for i in range(1,2**15)]), est_correlations_np) 	
-	plt.show()
-
-	print(f'Estimated initial state 2: {max(est_correlations_2)}')
+	#plt.plot(np.array([i for i in range(1,2**15)]), est_correlations_np, 'b^') 	
+	#plt.show()
 	
-	c2_init_state = est_correlations_2.index(max(est_correlations_2)) + 1
+	biggest_val = find_biggest_dev(est_correlations_2)
+	print(f'Estimated biggest correlation 2: {biggest_val}')
+	
+	c2_init_state = est_correlations_2.index(biggest_val) + 1
 	
 	est_correlations_3 = []
 	for init_state in range(1, 2**17):
@@ -130,16 +140,19 @@ if __name__ == '__main__':
 		correlation = 1 - hamming_distance/key_stream_length
 		est_correlations_3.append(correlation)	
 	est_correlations_np = np.array(est_correlations_3)	
-	# plt.plot(np.array([i for i in range(1,2**17)]), est_correlations_np) 	
-	# plt.show()
-	print(f'Estimated initial state 3: {max(est_correlations_3)}')
+	#plt.plot(np.array([i for i in range(1,2**17)]), est_correlations_np, 'r^') 	
+	#plt.show()
+	biggest_val = find_biggest_dev(est_correlations_3)
+	print(f'Estimated biggest correlation 3: {biggest_val}')
 	
-	c3_init_state = est_correlations_3.index(max(est_correlations_3)) + 1
+	c3_init_state = est_correlations_3.index(biggest_val) + 1
 	
 	u1 = np.array(lfsr(c1, [int(i) for i in '{0:013b}'.format(c1_init_state)], key_stream_length))
 	u2 = np.array(lfsr(c2, [int(i) for i in '{0:015b}'.format(c2_init_state)], key_stream_length))
 	u3 = np.array(lfsr(c3, [int(i) for i in '{0:017b}'.format(c3_init_state)], key_stream_length))
 	
 	sequence = majority_function([u1,u2,u3])
-	print("".join([str(elem) for elem in sequence]))
-	print(key_stream_str)
+	if "".join([str(elem) for elem in sequence]) == key_stream_str:
+		print(f'Attack complete -> K1={bin(c1_init_state)[2:]} K2=00{bin(c2_init_state)[2:]} K3={bin(c3_init_state)[2:]}')
+		
+
